4d540982cf9d5e3296e2f5ee01972029fd4739ee
stopopol <christoph.wohner@umweltbundesamt.at>
2021-03-24 11:29:36 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -13,39 +13,26 @@ Drupal.behaviors.deims_elter_so_formatter = {
 	  marker: {line: {width: 2}},
 	}]; 
 	
-	/*
-	var data = [{
-	  type: "sunburst",
-	  ids: [
-		"North America", "Europe", "Australia", "North America - Football", "Soccer",
-		"North America - Rugby", "Europe - Football", "Rugby",
-		"Europe - American Football","Australia - Football", "Association",
-		"Australian Rules", "Autstralia - American Football", "Australia - Rugby",
-		"Rugby League", "Rugby Union"
-	  ],
-	  labels: [
-		"North<br>America", "Europe", "Australia", "Football", "Soccer", "Rugby",
-		"Football", "Rugby", "American<br>Football", "Football", "Association",
-		"Australian<br>Rules", "American<br>Football", "Rugby", "Rugby<br>League",
-		"Rugby<br>Union"
-	  ],
-	  parents: [
-		"", "", "", "North America", "North America", "North America", "Europe",
-		"Europe", "Europe","Australia", "Australia - Football", "Australia - Football",
-		"Australia - Football", "Australia - Football", "Australia - Rugby",
-		"Australia - Rugby"
-	  ],
-	  outsidetextfont: {size: 20, color: "#377eb8"},
-	  // leaf: {opacity: 0.4},
-	  marker: {line: {width: 2}},
-	}]; */
+	var config = {
+		responsive: true,
+		displaylogo: false,
+		toImageButtonOptions: {
+			format: 'png', // one of png, svg, jpeg, webp
+			filename: 'elter_standard_observations',
+			height: 1200,
+			width: 1200,
+			scale: 1 // Multiply title/legend/axis/canvas sizes by this factor
+		},
+		modeBarButtonsToRemove: ['toggleHover']
+	}
 
 	var layout = {
+	  autosize: true,
 	  margin: {l: 0, r: 0, b: 0, t:0},
 	  sunburstcolorway:["#636efa","#ef553b","#00cc96"],
 	};
 
-      Plotly.newPlot('my_elter_so_test', data, layout, {displaylogo: false});
+	Plotly.newPlot('my_elter_so_test', data, layout, config);
 	
   }
 };
